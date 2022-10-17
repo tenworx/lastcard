ec50a5e97e74b5150d4733c1e385192771100762
huzi-hub <h.t1149171@gmail.com>
2022-10-17 22:38:17 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs, use_build_context_synchronously
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter_upload/webview_flutter.dart';
+
+void main() => runApp(
+    MaterialApp(debugShowCheckedModeBanner: false, home: WebViewExample()));
+
+class WebViewExample extends StatefulWidget {
+  @override
+  _WebViewExampleState createState() => _WebViewExampleState();
+}
+
+late WebViewController webctrl;
+
+class _WebViewExampleState extends State<WebViewExample> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  bool flag = false;
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Builder(builder: (BuildContext context) {
+          return WillPopScope(
+            onWillPop: () async {
+              if (await webctrl.canGoBack()) {
+                webctrl.goBack();
+                return false;
+              } else {
+                return false;
+              }
+            },
+            child: WebView(
+              initialUrl: 'http://www.lastcard.click/',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                webctrl = webViewController;
+                _controller.complete(webViewController);
+              },
+              onProgress: (int progress) {
+                print("WebView is loading (progress : $progress%)");
+              },
+              javascriptChannels: <JavascriptChannel>{
+                _toasterJavascriptChannel(context),
+              },
+              onPageStarted: (String url) {
+                print('Page started loading: $url');
+              },
+              onPageFinished: (String url) {
+                print('Page finished loading: $url');
+              },
+              gestureNavigationEnabled: true,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster', onMessageReceived: (JavascriptMessage message) {});
+  }
+}
